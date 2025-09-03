1e495ecfdc178a9ec32665efa3a181b529917d84
deepanshu kumar <deepanshukumar2580@gmail.com>
2025-09-04 04:27:33 +0530

    build files added

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import authRouter from "../routes/auth.routes.js";
 import chatRouter from "../routes/chat.routes.js";
+import path from "path";
 
 const app = express();
 app.use(cors({
@@ -12,9 +13,13 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, "../public")));
+
 app.use("/api/auth",authRouter)
 app.use("/api/chat",chatRouter)
 
-
+app.get("*name",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+})
 
 export default app;
